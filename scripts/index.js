41a2908f2989b760f814971c73bb4a9d732eef3b
Bryan Teh Yue Hung <kyle.19932@hotmail.com>
2021-01-04 21:38:23 +0800

    2138

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+function GetCast(addCast) {
+    let url = 'https://api.sheety.co/f69038c6ee1e7950b8ca2f5803dcd15a/castName/castname';
+    let body = {
+        castname: {
+            castName: addCast,
+        }
+    }
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then((response) => response.json())
+        .then(json => {
+            alert(json.booking.name + " added!");
+        });
+}
+
+window.addEventListener("load", function () {
+    document.getElementById("addCast").addEventListener("click", function () {
+        let addCast = document.getElementById("addCast").value;
+
+        GetCast(addCast);
+
+    });
+});
