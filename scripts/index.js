9ebe6f76e297bba77380d50c38e3bd36345bc43c
Bryan Teh Yue Hung <kyle.19932@hotmail.com>
2021-01-05 00:08:48 +0800

    0008

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
-function GetCast(addCast) {
+function CastList(addCast) {
     let url = 'https://api.sheety.co/f69038c6ee1e7950b8ca2f5803dcd15a/castName/castname';
     let body = {
-        CastName: {
-            castname: addCast,
+        castname: {
+            castName: addCast
         }
     }
     fetch(url, {
@@ -14,15 +14,42 @@ function GetCast(addCast) {
     })
         .then((response) => response.json())
         .then(json => {
-            alert(json.castname.addCast + " added!");
+            alert(json.castname.castName + " added!");
         });
 }
-//
+
 window.addEventListener("load", function () {
-    document.getElementById("addCast").addEventListener("click", function () {
+    document.getElementById("BtnCast").addEventListener("click", function () {
         let addCast = document.getElementById("addCast").value;
 
-        GetCast(addCast);
+        CastList(addCast);
 
     });
 });
+
+function GetCast() {
+    let url = 'https://api.sheety.co/f69038c6ee1e7950b8ca2f5803dcd15a/castName/castname';
+    fetch(url)
+        .then((response) => response.json())
+        .then(json => {
+            // Do something with the data
+            //console.log(json.bookings);
+            let castList = document.getElementById('cast-list');
+            let castName = [];
+
+
+
+            for (let i = 0; i < json.castname.length; i++) {
+                let cName = json.castname[i].castName;
+
+                let li = castList.insertRow(castList.rows.length);
+                li.innerHTML = "<li><a href='https://www.google.com/search?q=" + cName + "'>" + cName + "</a></li>"
+
+                castName.push(buttonId);
+            }
+        }
+window.addEventListener("load", function () {
+            document.getElementById("cast-list").addEventListener("click", function () {
+                GetCast();
+            });
+        });
