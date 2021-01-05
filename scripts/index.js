ddab9e2c1d0a2729df30e2ce66c2e0ebbb9920df
Bryan Teh Yue Hung <kyle.19932@hotmail.com>
2021-01-05 08:53:37 +0800

    0853

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
-function CastList(addCast) {
+function CastNow(addCast) {
     let url = 'https://api.sheety.co/f69038c6ee1e7950b8ca2f5803dcd15a/castName/castname';
     let body = {
         castname: {
-            castName: addCast
+            name: addCast
         }
     }
     fetch(url, {
@@ -14,7 +14,7 @@ function CastList(addCast) {
     })
         .then((response) => response.json())
         .then(json => {
-            alert(json.castname.castName + " added!");
+            alert(json.castname.name + " added!");
         });
 }
 
@@ -22,32 +22,7 @@ window.addEventListener("load", function () {
     document.getElementById("BtnCast").addEventListener("click", function () {
         let addCast = document.getElementById("addCast").value;
 
-        CastList(addCast);
+        CastNow(addCast);
 
     });
 });
-
-function GetCast() {
-    let url = 'https://api.sheety.co/f69038c6ee1e7950b8ca2f5803dcd15a/castName/castname';
-    fetch(url)
-        .then((response) => response.json())
-        .then(json => {
-            // Do something with the data
-            //console.log(json.bookings);
-            let castList = document.getElementById('cast-list');
-            let castName = [];
-
-
-
-            for (let i = 0; i < json.castname.length; i++) {
-                let cName = json.castname[i].castName;
-
-                let li = castList.insertRow(castList.rows.length);
-                li.innerHTML = "<li><a href='https://www.google.com/search?q=" + cName + "'>" + cName + "</a></li>"
-            }
-        }
-window.addEventListener("load", function () {
-            document.getElementById("cast-list").addEventListener("click", function () {
-                GetCast();
-            });
-        });
