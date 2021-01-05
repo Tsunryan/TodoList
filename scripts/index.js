580a40c965ed2896a22cbd328ed6ca59aa908bc8
Bryan Teh Yue Hung <kyle.19932@hotmail.com>
2021-01-05 11:12:30 +0800

    1112

--- scripts/index.js
+++ scripts/index.js
@@ -26,3 +26,27 @@ window.addEventListener("load", function () {
 
     });
 });
+
+function GetCast() {
+    let url = 'https://api.sheety.co/f69038c6ee1e7950b8ca2f5803dcd15a/castName/castname';
+    fetch(url)
+        .then((response) => response.json())
+        .then(json => {
+            // Do something with the data
+            //console.log(json.castname);
+            let castList = document.getElementById('cast-list');
+
+            for (let i = 0; i < json.castname.length; i++) {
+                let cName = json.bookings[i].name;
+
+
+                let row = castList.insertRow(castList.rows.length);
+                row.insertCell(0).innerHTML = "<li><a href='https://www.google.com/search?q=" + cName + "'></a></li>";
+
+            }
+        }
+window.addEventListener("load", function () {
+            document.getElementById("BtnCast").addEventListener("click", function () {
+                GetCast();
+            });
+        });
